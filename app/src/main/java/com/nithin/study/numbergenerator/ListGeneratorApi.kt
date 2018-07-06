c1d592719cf59652631e1835b7e399a38f4f4011
NithinMJ <30488140+NithinMJ@users.noreply.github.com>
2018-07-06 11:23:41 +0100

    Handling new scenario

---
+++ app/src/main/java/com/nithin/study/numbergenerator/ListGeneratorApi.kt
@@ -0,0 +1,14 @@
+package com.nithin.study.numbergenerator
+
+import java.util.*
+
+interface ListGeneratorApi {
+    fun getPeople(): List<Person> {
+        return when (Random().nextInt(4)) {
+            1 -> emptyList()
+            2 -> listOf(Person(name = "John", surname = "Doe"), Person(name = "Jane", surname = "Doe"))
+            else -> throw Exception()
+        }
+    }
+
+}
